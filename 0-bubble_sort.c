7f6d89de7d1d12d7dd9cf79999b0ade536977c8b
= <=>
2021-02-03 12:03:58 +0200

    check efficiency

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
+
 #include "sort.h"
 
 /**
-  *bubble_sort - sort algorithm.
-  *@array: the arrat to sort.
-  *@size: the size of the array.
-  */
+ *bubble_sort - sort algorithm.
+ *@array: the arrat to sort.
+ *@size: the size of the array.
+ */
 
-void bubble_sort(int *array, size_t size)
-{
-int swap, swapped;
-size_t i, j;
+void bubble_sort(int * array, size_t size) {
+    int swap, swapped;
+    size_t i, j;
 
-if (!array || array == NULL || size == 0)
-return;
+    if (!array || array == NULL || size == 0)
+        return;
 
-for (i = 0; i < size; i++)
-{
-swapped = 0;
-for (j = 0; j < size; j++)
-{
-if (array[j] > array[j + 1])
-{
-swap = array[j + 1];
-array[j + 1] = array[j];
-array[j] = swap;
-swapped = 1;
-}
-}
-print_array(array, size);
-if (swapped == 1)
-break;
-}
+    for (i = 0; i < size; i++) {
+        swapped = 0;
+        for (j = 0; j < size; j++) {
+            if (array[j] > array[j + 1]) {
+                swap = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = swap;
+                swapped = 1;
+            }
+            print_array(array, size);
+            if (swapped == 0 && i == size - 1)
+            {
+                break;
+            }
+        }
+    }
 }

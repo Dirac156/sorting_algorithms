5b8ca3f36559a89df6c8c1dc958f9e66d1391e7d
= <=>
2021-02-03 12:30:17 +0200

    nice printing

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,27 +8,25 @@
  *@size: the size of the array.
  */
 
-void bubble_sort(int * array, size_t size) {
-    int swap, swapped;
-    size_t i, j;
+void bubble_sort(int * array, size_t size) 
+{
+int swap;
+size_t i, j;
 
-    if (!array || array == NULL || size == 0)
-        return;
+if (!array || array == NULL || size == 0)
+return;
 
-    for (i = 0; i < size; i++) {
-        swapped = 0;
-        for (j = 0; j < size; j++) {
-            if (array[j] > array[j + 1]) {
-                swap = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = swap;
-                swapped = 1;
-            }
-            print_array(array, size);
-            if (swapped == 0 && i == size - 1)
-            {
-                break;
-            }
-        }
-    }
+for (i = 0; i < size; i++) 
+{
+for (j = 0; j < size; j++) 
+{
+if (array[j] > array[j + 1]) 
+{
+swap = array[j];
+array[j] = array[j + 1];
+array[j + 1] = swap;
+print_array(array, size);  
+}
+}
+}
 }

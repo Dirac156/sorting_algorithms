0efddeb666be2aa32e0aaba7321347d1e04f1a7c
= <=>
2021-02-03 12:55:45 +0200

    fix bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,30 +3,43 @@
 #include "sort.h"
 
 /**
+ *swap - wap two values
+ *@prev: the arrat to sort.
+ *@next: the size of the array.
+ */
+void swap(int *prev, int *next)
+{
+int swap_numb = *prev;
+*prev = *next;
+*next = swap_numb;
+}
+
+/**
  *bubble_sort - sort algorithm.
  *@array: the arrat to sort.
  *@size: the size of the array.
  */
 
-void bubble_sort(int * array, size_t size) 
+void bubble_sort(int *array, size_t size)
 {
-int swap;
+int swapped;
 size_t i, j;
 
-if (!array || array == NULL || size == 0)
+if (!array || size <= 0)
 return;
-
-for (i = 0; i < size; i++) 
+for (i = 0; i < size; i++)
 {
-for (j = 0; j < size; j++) 
+swapped = 0;
+for (j = 0; j < size; j++)
 {
-if (array[j] > array[j + 1]) 
+if (array[j] > array[j + 1])
 {
-swap = array[j];
-array[j] = array[j + 1];
-array[j + 1] = swap;
-print_array(array, size);  
+swap(&array[j], &array[j + 1]);
+swapped = 1;
+print_array(array, size);
 }
 }
+if (swapped == 0)
+return;
 }
 }
--- sort.h
+++ sort.h
@@ -19,4 +19,5 @@ void bubble_sort(int *array, size_t size);
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap(int *prev, int *next);
 #endif

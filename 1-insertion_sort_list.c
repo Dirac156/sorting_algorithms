dc3b2b7e56113beb1a3f3398a3f2e09d6b53e940
= <=>
2021-02-03 17:54:19 +0200

    insertion algo

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+
+#include "sort.h"
+
+/**
+ *insertion_sort_list - algorithm
+ * @list: the list to sort.
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+listint_t *next, *current, *prev;
+int inserted = 0;
+if (!(*list) || (*list)->next == NULL)
+return;
+current = *list, next = current->next;
+while (current->next)
+{
+inserted = 0;
+if (current->n > next->n)
+{
+inserted = 1;
+prev = current->prev;
+current->next = next->next;
+if (current->next)
+current->next->prev = current;
+current->prev = next;
+next->next = current;
+if (prev)
+{
+next->prev = prev;
+prev->next = next;
+}
+else
+next->prev = NULL;
+print_list(*list);
+if (current->prev)
+check_inner(current->prev, current->prev->prev, current, list);
+}
+next = current->next;
+if (inserted == 1 && next)
+{
+current = current;
+next = current->next;
+}
+else if (next)
+{
+current = next;
+next = current->next;
+}
+}
+}
+
+/**
+  *check_inner - cehck inner function.
+  *@cu: current node.
+  *@p: previous node.
+  *@n: next node.
+  *@h: head.
+  */
+
+void check_inner(listint_t *cu, listint_t *p, listint_t *n, listint_t **h)
+{
+listint_t *tmp;
+int i = 1;
+
+if (!cu || !p || !n || !(*h))
+return;
+
+while (i && (cu->n < p->n))
+{
+tmp = p->prev;
+p->next = n;
+p->prev = cu;
+cu->prev = tmp;
+cu->next = p;
+n->prev = p;
+if (tmp)
+tmp->next = cu;
+if (cu->prev)
+{
+n = p;
+p = cu->prev;
+}
+else
+{
+*h = cu;
+i = 0;
+}
+print_list(*h);
+}
+}
--- sort.h
+++ sort.h
@@ -1,6 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -16,8 +18,10 @@ struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap(int *prev, int *next);
+void check_inner(listint_t *, listint_t *, listint_t *, listint_t **);
 #endif
